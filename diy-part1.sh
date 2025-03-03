7041db50f4c5f5ad5099faacb69f22cbaa9f51cf
okgengkeja <okgenngswe12@hotmail.com>
2025-03-03 18:49:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
